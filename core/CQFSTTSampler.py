2586e06abdb52e5120852648d7338b120f970e95
Artyom Nikitin <a.nikitin@edadeal.ru>
2022-03-23 11:09:49 +0300

    feat: cuda

--- core/CQFSTTSampler.py
+++ core/CQFSTTSampler.py
@@ -1,7 +1,9 @@
 import tntorch as tn
 import torch
+import operator
 torch.set_default_dtype(torch.float64)
 DTYPE = torch.float64
+DEVICE = torch.device('cuda:2')
 
 
 class CQFSTTSampler:
@@ -10,16 +12,16 @@ class CQFSTTSampler:
     def __init__(self, tol: float = 1e-4):
         self._tol = tol
 
-    def _optimize(self, W, s, k, eps=1e-4):
+    def _optimize(self, W, s, k, eps=1e-6):
         N = W.shape[0]
         assert W.shape[1] == N
 
         # First term: x @ W @ x^T
         ts = []
         for n in range(N):
-            c = torch.zeros([1, 2, 1], dtype=DTYPE)
+            c = torch.zeros([1, 2, 1], dtype=DTYPE, device=DEVICE)
             c[0, 1, 0] = 1
-            t = tn.Tensor([c])[:, None]
+            t = tn.Tensor([c], device=DEVICE)[:, None]
             t.cores[1] = t.cores[1].repeat(1, N, 1)
             t.cores[1][:, :n, :] = 0
             t.cores[1][:, n + 1:, :] = 0
@@ -27,25 +29,26 @@ class CQFSTTSampler:
             t = t.repeat(*[2] * n, 1, *[2] * (N - n - 1), 1)
             ts.append(t)
         t = sum(ts)
-        t2 = t.clone()
-        t.cores[-1] = torch.einsum('ijl,jk->ikl', t.cores[-1], W)
-        term1 = tn.cross(tensors=[t, t2], function=lambda x, y: x * y,
-                         ranks_tt=N * 2 + 1, max_iter=50, eps=eps)
-        term1 = tn.sum(term1, dim=-1)
+        ts = []
+        for n in range(N):
+            trow = t.clone()
+            trow.cores[-1] = torch.einsum('ijl,jk->ikl', trow.cores[-1], W[:, n:n + 1])
+            ts.append(trow)
+        term1 = tn.reduce(ts, operator.add, eps=eps)[..., 0]
 
         # Second term
-        c = torch.eye(2, 2, dtype=DTYPE)[:, None, :].repeat(1, 2, 1)
+        c = torch.eye(2, 2, dtype=DTYPE, device=DEVICE)[:, None, :].repeat(1, 2, 1)
         c[1, 0, 0] = (-k) ** 2
         c[1, 1, 0] = (1 - k) ** 2
-        xs = tn.Tensor([c[-1:, :, :]] + [c] * (N - 2) + [c[:, :, 0:1]])
+        xs = tn.Tensor([c[-1:, :, :]] + [c] * (N - 2) + [c[:, :, 0:1]], device=DEVICE)
         term2 = s * tn.round_tt(xs, eps=eps)
 
         # Minimize tensor
         target = tn.round_tt(term1 + term2, eps=eps)
-        return tn.argmin(target)
+        return tn.argmin(target, rmax=50, verbose=True)
 
     def sample(self, *, FPM, s, k):
-        FPM = torch.tensor(FPM, dtype=DTYPE)
+        FPM = torch.tensor(FPM, dtype=DTYPE, device=DEVICE)
         f = self._optimize(FPM, s, k)
         return {
             i: f[i]
